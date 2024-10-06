c3b7a12b5fb7d5a6a6f0d2de63fb8065aa3d222f
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-10-06 09:56:16 -0700

    add change canvas size button

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const container = document.querySelector(".container");
+const divStack = [];
+
 let isLeftClick = false;
 
 function showGrid(size = 100) {
+  document.querySelector(".container").remove();
+  const container = document.createElement("div");
+  container.classList.toggle("container");
+
+  if (!size || size > 100) {
+    size = 100;
+  }
   const percentage = (1 / size) * 100;
   size = size ** 2;
 
@@ -11,20 +19,23 @@ function showGrid(size = 100) {
     div.style["width"] = `${percentage}%`;
     container.appendChild(div);
   }
+  container.addEventListener("mouseover", mouseoverHandler);
+  container.addEventListener("mousedown", (e) => {
+    isLeftClick = e.button === 0;
+  });
+
+  container.addEventListener("mouseup", () => {
+    isLeftClick = false;
+  });
+  document.body.append(container);
 }
 
 function mouseoverHandler(e) {
-  if (isLeftClick) e.target.style["background-color"] = "red";
+  if (isLeftClick) e.target.style.backgroundColor = "red";
 }
 
-container.addEventListener("mouseover", mouseoverHandler);
-
-container.addEventListener("mousedown", (e) => {
-  isLeftClick = e.button === 0;
+showGrid(100);
+changeGridSize.addEventListener("click", (e) => {
+  const divAmount = +prompt("Enter Grid Size: ");
+  showGrid(divAmount);
 });
-
-container.addEventListener("mouseup", () => {
-  isLeftClick = false;
-});
-
-showGrid();
