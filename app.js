f9180837a2f2696f44292d57e17fd3d995fc867a
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-10-06 08:18:35 -0700

    feat: add draw pixels when leftclick + hover

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const container = document.querySelector(".container");
+let isLeftClick = false;
 
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.toggle("grid");
-  container.appendChild(div);
+function showGrid(size = 100) {
+  const percentage = (1 / size) * 100;
+  size = size ** 2;
+
+  for (let i = 0; i < size; i++) {
+    const div = document.createElement("div");
+    div.classList.toggle("grid");
+    div.style["width"] = `${percentage}%`;
+    container.appendChild(div);
+  }
 }
+
+function mouseoverHandler(e) {
+  if (isLeftClick) e.target.style["background-color"] = "red";
+}
+
+container.addEventListener("mouseover", mouseoverHandler);
+
+container.addEventListener("mousedown", (e) => {
+  isLeftClick = e.button === 0;
+});
+
+container.addEventListener("mouseup", () => {
+  isLeftClick = false;
+});
+
+showGrid();
